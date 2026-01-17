4ccf58847a639b4c4ec8ce04663e2c84a8b5d659
Obad <apoeljzzar@gmail.com>
2026-01-17 20:11:36 +0200

    install pasport - create jwt strategy

---
+++ libs/auth/src/strategies/jwt.strategy.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from '../interfaces/iwt.payload.interfaces';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow<string>('JWT_SECRET') as string,
+    });
+  }
+
+  validate(payload: JwtPayload) {
+    if (!payload.sub) {
+      throw new UnauthorizedException();
+    }
+
+    return {
+      userId: payload.sub,
+      username: payload.username,
+      email: payload.email,
+    };
+  }
+}
