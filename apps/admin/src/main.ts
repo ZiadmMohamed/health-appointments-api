5661cc19282d0c25b9d842765ef528aac419368e
Ebtisam.elnahas20@gmail.com <Ebtisam.elnahas20@gmail.com>
2026-02-15 10:44:20 +0200

    resolve: conflict

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -1,25 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AdminModule } from './admin.module';
 import { ErrorHandlingInterceptor } from '@app/common/interceptor/error-handler.interceptor';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  
   const app = await NestFactory.create(AdminModule);
-  const configService = app.get(ConfigService);
 
   app.useGlobalInterceptors(new ErrorHandlingInterceptor());
-  const port = configService.get<number>('PORT') || 3000;
 
-  try {
-    await app.listen(port);
-    console.log(`🚀 Admin Server is running on port ${port}`);
-  } catch (error) {
-    console.error(`❌ Error starting server: ${error}`);
-    process.exit(1); 
-  }
+  await app.listen(process.env.ADMIN_PORT ?? 3001);
 }
-bootstrap().catch((err) => {
-  console.error('Critical error during bootstrap:', err);
-  process.exit(1);
-});
+bootstrap();
